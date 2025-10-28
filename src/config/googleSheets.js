bad23f0adeb438962f3ac8d3c22eb16c92c9c0d4
Khushal Sharma <khushal.sharma4416@gmail.com>
2025-10-28 21:55:50 +0530

    script url updated

--- src/config/googleSheets.js
+++ src/config/googleSheets.js
@@ -1,2 +1,3 @@
-export const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz2NUCBPsY8aloN-yHsCWpELuSbZoqpMEC1hQd3KFTjHDgr6jIBd5u1s_wmrgzqsNnBrQ/exec';
+// export const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz2NUCBPsY8aloN-yHsCWpELuSbZoqpMEC1hQd3KFTjHDgr6jIBd5u1s_wmrgzqsNnBrQ/exec';
 
+export const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxj-gpU6GktKB3Cog9KveDWab-x605e5jVIX-5uqCRUkEBoSxsn9g5of3wrIuAihmT5/exec';
